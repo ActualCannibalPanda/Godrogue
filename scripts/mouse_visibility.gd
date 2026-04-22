dd5664accb4ef8ccf83ff31e64cb01cc453281d6
Robert O'Shea <robertosheagamedev@gmail.com>
2026-04-22 20:39:18 +0100

    Increase radius of hiding tiles to 2

--- scripts/mouse_visibility.gd
+++ scripts/mouse_visibility.gd
@@ -25,9 +25,18 @@ func _process(_delta):
 				layer.set_cell(map_pos, layer.tile_set.get_source_id(0), layer.get_cell_atlas_coords(map_pos), 1)
 				hidden_cells.push_back(map_pos)
 
-		for neighbour in layer.get_surrounding_cells(map_pos):
-			var neighbour_data = layer.get_cell_tile_data(neighbour)
+		for n in layer.get_surrounding_cells(map_pos):
+			var neighbour_data = layer.get_cell_tile_data(n)
 			if neighbour_data:
 				if not neighbour_data.get_custom_data("walkable"):
-					layer.set_cell(neighbour, layer.tile_set.get_source_id(0), layer.get_cell_atlas_coords(neighbour), 1)
-					hidden_cells.push_back(neighbour)
+					layer.set_cell(n, layer.tile_set.get_source_id(0), layer.get_cell_atlas_coords(n), 1)
+					if not hidden_cells.has(n):
+							hidden_cells.push_back(n)
+					
+			for n2 in layer.get_surrounding_cells(n):
+				var n2_data = layer.get_cell_tile_data(n2)
+				if n2_data:
+					if not n2_data.get_custom_data("walkable"):
+						layer.set_cell(n2, layer.tile_set.get_source_id(0), layer.get_cell_atlas_coords(n2), 1)
+						if not hidden_cells.has(n2):
+							hidden_cells.push_back(n2)
